745de01ec81d9aac8e1c66ecf43dc3b20edf3e29
zhonghanlu <1420865757@qq.com>
2022-08-14 22:07:20 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
+var babel = require('gulp-babel');
+
+// 压缩css文件
+gulp.task('minify-css', function (done) {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+
+// 压缩html文件
+gulp.task('minify-html', function (done) {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'));
+    done();
+});
+
+// 压缩js文件
+gulp.task('minify-js', function (done) {
+    return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+        .pipe(babel({
+            //将ES6代码转译为可执行的JS代码
+            presets: ['es2015'] // es5检查机制
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+
+// 压缩 public/images 目录内图片(Version>3)
+gulp.task('minify-images', function (done) {
+    gulp.src('./public/images/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/images'));
+    done();
+});
+
+//4.0以后的写法
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js', 'minify-images')), function () {
+    console.log("----------gulp Finished----------");
+    // Do something after a, b, and c are finished.
+});
